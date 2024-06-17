5284326a33d0a1de34790148a7054fe23e8137e5
Oleksii Fedorenko <lankrf@gmail.com>
2024-06-18 00:57:43 +0300

    Add php docs for TestController

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -4,11 +4,17 @@ namespace App\Controllers;
 
 use Bubblegum\Controller;
 
+/**
+ * Controller for testing (Don't use it on your production pls)
+ */
 class TestController extends Controller
 {
+    /**
+     * Testing json response for post http method
+     * @return array|string
+     */
     public function store(): array|string
     { // "store" is a default destination for post http method
         return ['data' => 'Hello folks, this is a test information here. I am Bubblegum btw.'];
     }
-
 }
