84e76980e7e664c6f045a82a73ce9fef2678c38d
Oleksii Fedorenko <lankrf@gmail.com>
2024-09-20 22:18:09 +0300

    Change welcome.php and additional data

--- app/routes.php
+++ app/routes.php
@@ -9,5 +9,12 @@ use Bubblegum\Views\View;
 use App\Controllers\TestController;
 
 // Bubblegum routes down here
-Route::get('/', View::class)->to('welcome');
+Route::get('/', View::class)->to('welcome')
+    ->withData([
+        'message' =>    'Just like stars twinkle in the night sky, your ideas are shining bright! ' .
+                        'And yes, this is a test message. This message has been added from routes.php ' .
+                        'for testing purposes. You can also add a message here by passing a data array to the view!',
+        'username' => 'User',
+        'link' => 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    ]);
 Route::post('/', TestController::class);
